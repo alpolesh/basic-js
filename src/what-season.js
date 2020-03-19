46e159004389728be662a92a72b565aed1f01a03
Andrei Paleshchuk <alpoleshuck@gmail.com>
2020-03-19 21:10:43 +0300

    feat: add object checking

--- src/what-season.js
+++ src/what-season.js
@@ -2,6 +2,9 @@ function getSeason(date) {
   if(date == undefined){
     return 'Unable to determine the time of year!';
   }
+  if(Object.prototype.toString.call(date) !== '[object Date]'){
+    throw new Error;
+  }
 
   let time = {
     0: "winter",
